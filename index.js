78da4c881786c9551c49992c2dcb8776448cddda
pranav <katkarpranav4@gmail.com>
2024-01-24 19:28:56 +0530

    added json data in list using import

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world!");
+import data from "./data.json" assert { type: "json" };
+// console.log(data);
+
+const list = document.getElementById("yo");
+data.map((element) => {
+  console.log(element);
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `${element.firstName} ${element.lastName}`;
+  list.appendChild(listItem);
+});
