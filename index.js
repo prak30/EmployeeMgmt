d5a118f1e5920a1f8b68be742c5788dc07883ce8
pranav <katkarpranav4@gmail.com>
2024-01-25 03:31:37 +0530

    add employee and delete employee functionality done

--- index.js
+++ index.js
@@ -8,11 +8,56 @@
   const employeeList = document.querySelector(".employees__names--list");
   const employeeInfo = document.querySelector(".employees__single--info");
 
+  const createEmployee = document.querySelector(".createEmployee");
+  const addEmployeeModal = document.querySelector(".addEmployee");
+  const addEmployeeForm = document.querySelector(".addEmployee_create");
+
+  createEmployee.addEventListener("click", () => {
+    addEmployeeModal.style.display = "flex";
+  });
+
+  addEmployeeModal.addEventListener("click", (e) => {
+    if (e.target.className === "addEmployee") {
+      addEmployeeModal.style.display = "none";
+    }
+  });
+
+  addEmployeeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(addEmployeeForm);
+    const values = [...formData.entries()];
+    let empData = {};
+    console.log(values);
+
+    values.forEach((value) => {
+      empData[value[0]] = value[1];
+    });
+    empData.id = employees[employees.length - 1].id + 1;
+    empData.imageUrl =
+      empData.imageUrl || "https://cdn-icons-png.flaticon.com/512/0/93.png";
+    employees.push(empData);
+    renderEmployees();
+    addEmployeeForm.reset();
+    addEmployeeModal.style.display = "none";
+  });
+
   employeeList.addEventListener("click", (e) => {
     if (e.target.tagName === "SPAN" && selectedEmployeeId !== e.target.id) {
       selectedEmployeeId = e.target.id;
       renderEmployees();
       renderSingleEmployee();
+    }
+
+    if (e.target.tagName === "I") {
+      employees = employees.filter(
+        (emp) => String(emp.id) !== e.target.parentNode.id
+      );
+      if (String(selectedEmployeeId) === e.target.parentNode.id) {
+        selectedEmployeeId = employees[0]?.id || -1;
+        selectedEmployee = employees[0] || {};
+        renderSingleEmployee();
+      }
+      renderEmployees();
     }
   });
 
