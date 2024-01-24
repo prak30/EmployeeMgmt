19c5e5129774716ed143147d1b04ec226272fad5
pranav <katkarpranav4@gmail.com>
2024-01-24 21:12:17 +0530

    Added employees to employee list and styled them

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-fetch("./data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    // console.log(data);
-    const list = document.getElementById("yo");
+(async function () {
+  const data = await fetch("./data.json");
+  const res = await data.json();
+  // console.log(res);
+  let employees = res;
+  let selectedEmployeeId = employees[0].id;
+  let selectedEmployee = employees[0];
 
-    data.map((element) => {
-      console.log(element);
-      const li = document.createElement("li");
-      li.innerHTML = `${element.firstName} ${element.lastName}`;
-      list.appendChild(li);
+  const employeeList = document.querySelector(".employees__names--list");
+  const employeeInfo = document.querySelector(".employees__single--list");
+
+  const renderEmployees = () => {
+    employeeList.innerHTML = "";
+    employees.forEach((emp) => {
+      const employee = document.createElement("span");
+      employee.classList.add("employees__names--item");
+      if (selectedEmployeeId === emp.id) {
+        employee.classList.add("selected");
+        selectedEmployee = emp;
+      }
+      employee.setAttribute("id", emp.id);
+      employee.innerHTML = `${emp.firstName} ${emp.lastName} <i class="employeeDelete">❌</i>`;
+      employeeList.appendChild(employee);
     });
-  })
-  .catch((error) => console.error("Error loading JSON file", error));
+  };
+  renderEmployees();
+})();
